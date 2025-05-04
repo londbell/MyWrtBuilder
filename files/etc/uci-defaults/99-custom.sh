89cea8080015110ee68ca1398131485813798d30
londbell <tmacfeng@yeah.net>
2025-05-04 11:20:26 +0800

    feat: 🎸 update with bug

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -8,6 +8,10 @@ uci add dhcp domain
 uci set "dhcp.@domain[-1].name=time.android.com"
 uci set "dhcp.@domain[-1].ip=203.107.6.88"
 
+
+uci set network.lan.ipaddr='192.168.2.1'
+uci set network.lan.netmask='255.255.255.0'
+
 # https://github.com/nczyw/build_immortalwrt_r2s/blob/main/99-default-settings
 chmod 0755 /etc/init.d/fa-rk3328-pwmfan
 chmod 0755 /usr/bin/start-rk3328-pwm-fan.sh
